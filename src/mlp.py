dfdf25654bb22c65cf567188960e8015b24e0201
kristsam <xristostsatsabas@gmail.com>
2021-04-04 03:20:01 +0300

    class refactor +validation split

--- src/mlp.py
+++ src/mlp.py
@@ -1,8 +1,70 @@
 import math
 import numpy as np
-import read as r
 
-def cost_gradient(t, y, w1, w2, l):
+class MultiLayerPerceptron:
+    def __init__(self):
+        self.M = []
+        self.h = []
+
+    def add(self, m, activation):
+        self.M.append(m)
+        self.h.append(activation)
+
+    def compile(self, learning_rate=0.01, l=0.01):
+        self.hita = learning_rate
+        self.l = 0.01
+
+    def __split(self, x, validation_split):
+        split_rows = int(x.shape[0]* (1-validation_split))
+        return x[:split_rows, :], x[split_rows:, :]
+
+    def fit(self, x, y, batch_size, validation_split=0.2):
+        self.x_train, self.x_validation = self.__split(x, validation_split)
+        self.y_train, self.y_validation = self.__split(y, validation_split)
+        self.N = self.x_train.shape[0]
+        self.D = self.x_train.shape[1]
+        self.Nb = batch_size
+        self.x_train = np.insert(self.x_train, 0, 1, axis=1)
+        print(self.N, self.D)
+        # one inside layer
+        self.M = self.M[0]
+        self.K = y.shape[1]
+
+        y_out = np.empty(shape=(self.Nb,self.K))
+
+        w1 = 0.01*np.random.rand(self.M, self.D+1) + 0.001
+
+        z = np.empty(shape=(self.Nb, self.M+1))
+
+        w2 = 0.01*np.random.rand(self.K, self.M+1) + 0.001
+
+        for num1 in range(0, int(self.N/self.Nb)+1, self.Nb):
+            # each batch
+            for num2 in range(0, self.Nb):
+                n = num1 + num2
+                z[num2][0] = 1
+                for j in range(1,self.M+1):
+                    mult = np.dot(np.transpose(w1[j-1]), self.x_train[n])
+                    z[num2][j] = self.h[0](mult)
+
+                for k in range(0,self.K):
+                    y_out[num2][k] = softmax(k, w2, z[num2])
+
+            cost, gradient_w1, gradient_w2 = cost_gradient(self.y_train[num1:num1+self.Nb],y_out,w1,w2,z,self.l)
+            w2 = w2 - self.hita*gradient_w2
+            w1 = w1 - self.hita*gradient_w1
+
+    # TODO predict
+
+    def summary(self):
+        print("N =",self.N," D =",self.D," M =",self.M," K =",self.K," Nb =",self.Nb," l =",self.l)
+        print("x training set shape =", self.x_train.shape)
+        print("y training set shape =", self.y_train.shape)
+
+
+
+
+def cost_gradient(t, y, w1, w2, z, l):
     K = y.shape[0]
     N = y.shape[1]
     cost1 = 0
@@ -73,48 +135,3 @@ def choose_func():
 # K classes
 # Nb minibatch 
 # l learning rate, l>=0
-
-h = choose_func()
-
-import os 
-path_to_data = os.path.dirname(os.path.realpath(__file__))+'\\..\\data\\mnist\\'
-x_train, y_train, x_validation, y_validation, x_test, y_test = r.read(path_to_data)
-
-N = x_train.shape[0]
-D = x_train.shape[1]
-print(N, D)
-M = 100
-K = 10
-Nb = 100
-l = 0.01
-
-print("N =",N," D =",D," M =",M," K =",K," Nb =",Nb," l =",l)
-
-x_train = np.insert(x_train, 0, 1, axis=1)
-print("x training set shape =", x_train.shape)
-print("y training set shape =", y_train.shape)
-
-y = np.empty(shape=(Nb,K))
-
-w1 = 0.01*np.random.rand(M, D+1) + 0.001
-
-z = np.empty(shape=(Nb, M+1))
-
-w2 = 0.01*np.random.rand(K, M+1) + 0.001
-
-for num1 in range(0, int(N/Nb)+1, Nb):
-    # each batch
-    for num2 in range(0, Nb):
-        n = num1 + num2
-        z[num2][0] = 1
-        for j in range(1,M+1):
-            mult = np.dot(np.transpose(w1[j-1]), x_train[n])
-            z[num2][j] = h(mult)
-
-        for k in range(0,K):
-            y[num2][k] = softmax(k, w2, z[num2])
-
-    cost, gradient_w1, gradient_w2 = cost_gradient(y_train[num1:num1+Nb],y,w1,w2,l)
-    w2 = w2 - l*gradient_w2
-    w1 = w1 - l*gradient_w1
-
