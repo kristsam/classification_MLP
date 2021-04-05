1f13511579cb7fa7231d475edf59dfd9a7752e64
kristsam <xristostsatsabas@gmail.com>
2021-04-06 00:28:52 +0300

    multi-parameter ,diagrams TODO fix error

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ import mlp
 import os
 import read as r
 
-h = mlp.choose_func()
 path_to_data = os.path.dirname(os.path.realpath(__file__))+'\\..\\data\\mnist\\'
+d = 0
+diagram_file = os.path.dirname(os.path.realpath(__file__))+'\\..\\diagrams\\diagram'+str(d)+'.png'
+d =+ 1
 x_train, y_train, x_test, y_test = r.read(path_to_data)
-mlp = mlp.MultiLayerPerceptron()
-mlp.add(100,h)
-mlp.compile(0.01, 0.01)
-mlp.fit(x_train, y_train, 100, 0.2)
-#mlp.predict()
+model = mlp.MultiLayerPerceptron()
+model.add(100,activation='h1')
+model.compile([0.01], [0.01])
+model.fit(x_train, y_train, 100, 3, 0.2, report=diagram_file)
+#model.predict()
--- src/mlp.py
+++ src/mlp.py
@@ -1,67 +1,180 @@
 import math
 import numpy as np
+import sys
+import matplotlib.pyplot as plt
 
 class MultiLayerPerceptron:
+
+    VALID_FUNCTIONS = ['h1','h2','h3']
+
     def __init__(self):
         self.M = []
         self.h = []
+        self.learning_rate = []
+
+    def __choose_func(self, activation):
+        if activation == self.VALID_FUNCTIONS[0]:
+            h = h1_act
+        elif activation == self.VALID_FUNCTIONS[1]:
+            h = h2_act
+        elif activation== self.VALID_FUNCTIONS[2]: 
+            h = h3_act
+        else: 
+            if activation not in self.VALID_FUNCTIONS:
+                raise ValueError("results: activation function must be one of %r." % self.VALID_FUNCTIONS)
+        return h
 
     def add(self, m, activation):
-        self.M.append(m)
-        self.h.append(activation)
+        '''
+        Parameters
+        ----------
+        m (int):
 
-    def compile(self, learning_rate=0.01, l=0.01):
-        self.hita = learning_rate
-        self.l = 0.01
+        activation (String): {'h1', 'h2', 'h3'} There are 3 activation functions:
+        1. log(1+exp(a))
+        2. (exp(a)-exp(-a))/(exp(a)+exp(-a))
+        3. cos(a)
+        '''
+        self.M.append(m)
+        self.h.append(self.__choose_func(activation))
+
+    def compile(self, learning_rate=[0.01], l=[0.01]):
+        if isinstance(learning_rate, list):
+              self.learning_rate = learning_rate
+        elif isinstance(learning_rate, int):
+              self.learning_rate = [learning_rate]
+        if isinstance(l, list):
+             self.lam = l
+        elif isinstance(l, int):
+             self.lam = [l]
 
     def __split(self, x, validation_split):
         split_rows = int(x.shape[0]* (1-validation_split))
         return x[:split_rows, :], x[split_rows:, :]
 
-    def fit(self, x, y, batch_size, validation_split=0.2):
+    def save(self, error_in_epochs_list, learning_rate, lam, error_best, epoch_best, learning_rate_best, lam_best, report):
+        # TODO learning values and lam value legend
+        fig = plt.figure()
+        
+        fig.suptitle("Multilayer Perceptron")
+
+        for i in range(0, len(learning_rate)):
+            for j in range(0,len(lam)):
+                plt.subplot(len(learning_rate), len(lam), j*len(lam) + j+1)
+                plt.plot(error_in_epochs_list[i, j, :, 0]+1,error_in_epochs_list[i, j, :, 1], label="training data")
+                plt.plot(error_in_epochs_list[i, j, :, 0]+1,error_in_epochs_list[i, j, :, 2], label="validation data")
+                plt.ylabel('error')
+                plt.xlabel('epochs')
+                plt.axis([1, error_in_epochs_list[i, j, -1, 0]+1, 0, 1])
+
+        plt.legend()
+        fig.savefig(report)
+        return
+
+    def predict(self, x, w1=None, w2=None, h=None, fixed=False):
+        if not fixed:
+            x_new = np.insert(x, 0, 1, axis=1)
+            w1 = self.w1
+            w2 = self.w2
+            h = self.h
+        else:
+            x_new = np.copy(x)
+        y = np.zeros(shape=(x_new.shape[0], self.K))
+        z = np.zeros(shape=(self.M+1,1))
+        for i in range(0,x_new.shape[0]):
+            z[0][0] = 1
+            for j in range(1,self.M+1):
+                z[j][0] = h(np.dot(np.transpose(w1[j-1]), x_new[i]))
+            for k in range(0,self.K):
+                y[i][k] = softmax(k, w2, z)
+        return y
+
+    def score(self, y, t):
+        count = 0
+        for i in range(0,t.shape[0]):
+            index_max = 0
+            for j in range(0,t.shape[1]):
+                if y[i][index_max] < y[i][j]:
+                    index_max = j
+            if t[i][index_max] == 1:
+                count += 1
+        return 1-count/t.shape[0]
+
+    def keep_the_best_fit(self, M, N, Nb, learning_rate, lam, epochs, h, report=None):
+        D = self.D
+        K = self.K
+        
+        error_best = sys.maxsize
+        epoch_best = -1
+        learning_rate_best = -1
+        lam_best = -1
+        w1_best = 0.01*np.random.rand(M, D+1) + 0.001
+        w2_best = 0.01*np.random.rand(K, M+1) + 0.001
+
+        y_out = np.empty(shape=(Nb,K))
+        z = np.empty(shape=(Nb, M+1))
+
+        error_in_epochs_list = np.empty(shape=(len(learning_rate), len(lam), epochs, 3))
+        for lr in range(0,len(learning_rate)):
+            for l in range(0,len(lam)):
+                w1 = 0.01*np.random.rand(M, D+1) + 0.001
+                w2 = 0.01*np.random.rand(K, M+1) + 0.001
+                for epoch in range(0,epochs):
+                    for num1 in range(0, int(N/Nb)+1, Nb):
+                        # each batch
+                        for num2 in range(0, Nb):
+                            n = num1 + num2
+                            z[num2][0] = 1
+                            for j in range(1,M+1):
+                                mult = np.dot(np.transpose(w1[j-1]), self.x_train[n])
+                                z[num2][j] = h(mult)
+
+                            for k in range(0,self.K):
+                                y_out[num2][k] = softmax(k, w2, z[num2])
+
+                        cost, gradient_w1, gradient_w2 = cost_gradient(self.y_train[num1:num1+Nb],y_out,w1,w2,z,lam[l])
+                        w2 = w2 - learning_rate[lr]*gradient_w2
+                        w1 = w1 #- learning_rate[lr]*gradient_w1
+
+                    # predict and return error
+                    predictions_training = self.predict(self.x_train, w1, w2, h, fixed=True)
+                    predictions_validation = self.predict(self.x_validation, w1, w2, h, fixed=True)
+                    error_training = self.score(predictions_training, self.y_train)
+                    error_validation = self.score(predictions_validation, self.y_validation)
+                    error_in_epochs_list[lr][l][epoch][0] = epoch
+                    error_in_epochs_list[lr][l][epoch][1] = error_training
+                    error_in_epochs_list[lr][l][epoch][2] = error_validation
+
+                    if error_best > error_validation:
+                        error_best = error_validation
+                        w1_best = np.copy(w1)
+                        w2_best = np.copy(w2)
+                        epoch_best = -1
+                        learning_rate_best = learning_rate[lr]
+                        lam_best = lam[l]
+
+        if report is not None:
+            self.save(error_in_epochs_list, learning_rate, lam, error_best, epoch_best, learning_rate_best, lam_best, report)
+        return w1_best, w2_best, learning_rate_best, lam_best
+
+    def fit(self, x, y, batch_size, epochs, validation_split=0.2, report=None):
         self.x_train, self.x_validation = self.__split(x, validation_split)
         self.y_train, self.y_validation = self.__split(y, validation_split)
         self.N = self.x_train.shape[0]
         self.D = self.x_train.shape[1]
         self.Nb = batch_size
         self.x_train = np.insert(self.x_train, 0, 1, axis=1)
-        print(self.N, self.D)
-        # one inside layer
-        self.M = self.M[0]
+        self.x_validation = np.insert(self.x_validation, 0, 1, axis=1)
         self.K = y.shape[1]
-
-        y_out = np.empty(shape=(self.Nb,self.K))
-
-        w1 = 0.01*np.random.rand(self.M, self.D+1) + 0.001
-
-        z = np.empty(shape=(self.Nb, self.M+1))
-
-        w2 = 0.01*np.random.rand(self.K, self.M+1) + 0.001
-
-        for num1 in range(0, int(self.N/self.Nb)+1, self.Nb):
-            # each batch
-            for num2 in range(0, self.Nb):
-                n = num1 + num2
-                z[num2][0] = 1
-                for j in range(1,self.M+1):
-                    mult = np.dot(np.transpose(w1[j-1]), self.x_train[n])
-                    z[num2][j] = self.h[0](mult)
-
-                for k in range(0,self.K):
-                    y_out[num2][k] = softmax(k, w2, z[num2])
-
-            cost, gradient_w1, gradient_w2 = cost_gradient(self.y_train[num1:num1+self.Nb],y_out,w1,w2,z,self.l)
-            w2 = w2 - self.hita*gradient_w2
-            w1 = w1 - self.hita*gradient_w1
-
-    # TODO predict
+        # NOTE one inside layer
+        self.M = self.M[0]
+        self.h = self.h[0]
+        self.w1, self.w2, self.learning_rate_chosen, self.lam_chosen = self.keep_the_best_fit(self.M, self.N, self.Nb, self.learning_rate, self.lam, epochs, self.h, report)
 
     def summary(self):
-        print("N =",self.N," D =",self.D," M =",self.M," K =",self.K," Nb =",self.Nb," l =",self.l)
+        print("N =",self.N," D =",self.D," M =",self.M," K =",self.K," Nb =",self.Nb," l =", self.lam)
         print("x training set shape =", self.x_train.shape)
         print("y training set shape =", self.y_train.shape)
-
-
 
 
 def cost_gradient(t, y, w1, w2, z, l):
@@ -72,9 +185,9 @@ def cost_gradient(t, y, w1, w2, z, l):
         for n in range(0,y.shape[0]):
             cost1 += t[n][k]*np.log(y[n][k]) - l/2*np.linalg.norm(w2)**2
     cost = np.sum(np.dot(np.transpose(t),np.log(y)) - l/2*np.linalg.norm(w2)**2)
-    gradient_w1 = np.dot(np.transpose(t - y),z) - l*w2
-    # TODO calc gradient for W2
-    gradient_w2 = 0
+    gradient_w2 = np.dot(np.transpose(t - y),z) - l*w2
+    # TODO calc gradient for W1
+    gradient_w1 = 0
     return cost, gradient_w1, gradient_w2
 
 def h1_act(a):
@@ -112,22 +225,6 @@ def softmax(k, w, zn):
     if s!=0:
         return numerator/s
     return 0
-
-def choose_func():
-    ch = 0 
-    while ch<1 or ch>3:
-        print("Which one activation function do you want to choose?")
-        print("1. log(1+exp(a))")
-        print("2. (exp(a)-exp(-a))/(exp(a)+exp(-a))")
-        print("3. cos(a)")
-        ch = int(input("Give the apriopriate number: "))
-    if ch == 1:
-        h = h1_act
-    elif ch == 2:
-        h = h2_act
-    else:
-        h = h3_act
-    return h
 
 
 # N training examples, D characteristics
