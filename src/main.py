2c555298e8d82b00b24a1dc617ebad564b1ec915
kristsam <xristostsatsabas@gmail.com>
2021-04-15 23:01:18 +0300

    multiples

--- src/main.py
+++ src/main.py
@@ -3,12 +3,23 @@ import os
 import read as r
 
 path_to_data = os.path.dirname(os.path.realpath(__file__))+'\\..\\data\\mnist\\'
-d = 0
-diagram_file = os.path.dirname(os.path.realpath(__file__))+'\\..\\diagrams\\diagram'+str(d)+'.png'
-d =+ 1
+diagram_file = os.path.dirname(os.path.realpath(__file__))+'\\..\\diagrams\\'
 x_train, y_train, x_test, y_test = r.read(path_to_data)
-model = mlp.MultiLayerPerceptron()
-model.add(100,activation='h3')
-model.compile([0.01], [0.01])
-model.fit(x_train, y_train, batch_size=100, epochs=3, validation_split=0.2, report=diagram_file)
-#model.predict()
+
+M = [100,200,300]
+h_act =['h1','h2','h3']
+NB = [100,200]
+test_error = []
+
+for h in h_act: 
+    for m in M:
+        for nb in NB:
+            model = mlp.MultiLayerPerceptron()
+            model.add(m,activation=h)
+            model.compile([0.0001,0.00001], [0.0001])
+            model.fit(x_train, y_train, batch_size=nb, epochs=50, validation_split=0.2, report=diagram_file)
+            y_test_predictions = model.predict(x_test)
+            test_error.append(model.score(y_test_predictions, y_test))
+
+for i in range(0,len(test_error)):
+    print('Test '+str(i)+' error='+str(test_error[i])+'.')
