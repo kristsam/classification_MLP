daed5681c14a58620324bcf7f671c89ce53a6851
kristsam <xristostsatsabas@gmail.com>
2021-04-16 22:33:49 +0300

    true functions name

--- src/func.py
+++ src/func.py
@@ -2,26 +2,26 @@ import numpy as np
 import math
 
 '''softplus'''
-def h1_act(a):
+def softplus(a):
     h = np.log(1+np.exp(a))
     # h = 100 + math.log(math.exp(-100)+ math.exp(a-100))
     return h
 
-def h1_act_derivative(a):
+def softplus_derivative(a):
     return np.exp(a)/(1+np.exp(a))
 
 '''tanh'''
-def h2_act(a):
+def tanh(a):
     return (np.exp(a) - np.exp(-a)) / (np.exp(a) + np.exp(-a))
 
-def h2_act_derivative(a):
-    return 1 - h2_act(a)**2
+def tanh_derivative(a):
+    return 1 - tanh(a)**2
 
 '''cos'''
-def h3_act(a):
+def cos(a):
     return np.cos(a)
 
-def h3_act_derivative(a):
+def cos_derivative(a):
     return -np.sin(a)
 
 def mult_derivative(x,a):
@@ -35,12 +35,8 @@ def softmax(x, ax=1):
     p = np.exp( x - m )
     return ( p / np.sum(p,axis=ax,keepdims=True) )
 
+def softmax_derivative(x):
+    return softmax(x)(1-softmax(x))
+
 def h_act_to_string(h):
-    t = ''
-    if h == h1_act:
-        t = 'softplus'
-    elif h == h2_act:
-        t = 'tanh'
-    elif h == h3_act:
-        t = 'cos'
-    return t
+    return h.__name__
