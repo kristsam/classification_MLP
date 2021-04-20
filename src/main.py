fe9436d84fd63a881cc4519efcf9a126e0c3039f
kristsam <xristostsatsabas@gmail.com>
2021-04-20 18:38:30 +0300

    cifar-10 added

--- src/main.py
+++ src/main.py
@@ -5,24 +5,34 @@ import layers
 
 start = time.time()
 
-path_to_data = os.path.dirname(os.path.realpath(__file__))+'\\..\\data\\mnist\\'
+CIFAR_10 = 'cifar-10'
+MNIST = 'mnist'
+
+path_to_data = os.path.dirname(os.path.realpath(__file__))+'\\..\\data\\'
 diagram_file = os.path.dirname(os.path.realpath(__file__))+'\\..\\diagrams\\'
-x_train, y_train, x_test, y_test = r.read(path_to_data)
+diagram_files = [diagram_file+CIFAR_10+'\\', diagram_file+MNIST+'\\']
+
+data_list = []
+data_list.append(r.read_cifar_10(path_to_data+CIFAR_10+'\\'))
+data_list.append(r.read(path_to_data+MNIST+'\\'))
+
 
 M = [100,200,300]
 h_act =['softplus','tanh','cos']
 NB = [100,200]
 test_error = []
 
-for h in h_act: 
-    for m in M:
-        for nb in NB:
-            model = mlp.MultiLayerPerceptron()
-            model.add(layers.HiddenLayer(m, activation=h, add_column=True))
-            model.compile([0.0001,0.00001], [0.00001])
-            model.fit(x_train, y_train, batch_size=nb, epochs=50, validation_split=0.2, report=diagram_file)
-            y_test_predictions = model.predict(x_test)
-            test_error.append(model.score(y_test_predictions, y_test))
+# for each dataset
+for i in range(0, len(data_list)):
+    for h in h_act: 
+        for m in M:
+            for nb in NB:
+                model = mlp.MultiLayerPerceptron()
+                model.add(layers.HiddenLayer(m, activation=h, add_column=True))
+                model.compile([0.0001,0.00001], [0.00001])
+                model.fit(data_list[i][0], data_list[i][1], batch_size=nb, epochs=50, validation_split=0.2, report=diagram_files[i])
+                y_test_predictions = model.predict(data_list[i][2])
+                test_error.append(model.score(y_test_predictions, data_list[i][3]))
 
 for i in range(0,len(test_error)):
     print('Test '+str(i)+' error='+str(test_error[i])+'.')
--- src/read.py
+++ src/read.py
@@ -45,5 +45,36 @@ def read(path):
     y_train = np.array(y_train)
     x_test = np.array(x_test)
     y_test = np.array(y_test)
+    print('MNIST dataset read and ready for use.\n')
 
+    return x_train, y_train, x_test, y_test
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def read_cifar_10(path):
+    FILE_NAME_TRAIN = 'data_batch_'
+    TEST_BATCH = 'test_batch'
+    x_train = []
+    y_train = []
+    for i in range(1,6):
+        batch = unpickle(path+FILE_NAME_TRAIN+str(i))
+        if i == 1:
+            x_train =  batch[b'data']
+            y_train = batch[b'labels']
+        else:
+            x_train = np.concatenate((x_train, batch[b'data']))
+            y_train = np.concatenate((y_train, batch[b'labels']))
+
+    x_test = unpickle(path+TEST_BATCH)[b'data']
+    y_test = np.array(unpickle(path+TEST_BATCH)[b'labels'])
+
+    y_train = one_hot_vector(y_train)
+    y_test = one_hot_vector(y_test)
+
+    print('CIFAR_10 dataset read and ready for use.\n')
     return x_train, y_train, x_test, y_test
